5df39895afcfe511d0b92e89f99de672e4c34872
Alexander <alexandermichaelmiranda@gmail.com>
2015-02-07 09:07:03 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+requirejs.config({
+  baseUrl: 'js/',
+  paths: {
+    jquery: 'lib/jquery/dist/jquery.min',
+    underscore: 'lib/underscore/underscore-min',
+    backbone: 'lib/backbone/backbone',
+    leaflet: 'lib/leaflet/dist/leaflet'
+  }
+})
